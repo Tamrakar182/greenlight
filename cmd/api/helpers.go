2f08bdde3a2a9c44278b8df9f877b6d29deac737
Tamrakar182 <manjultamrakar4@gmail.com>
2025-06-17 21:16:35 +0545

    completed ch 15

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -135,6 +135,8 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 }
 
 func (app *application) background(fn func()) {
+	app.wg.Add(1)
+	
 	go func() {
 		defer app.wg.Done()
 
